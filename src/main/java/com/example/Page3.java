ee376dca88a214a085707e21bd38b2e26b15a9cd
rahuljayant <rahul.jayant@gmail.com>
2019-02-22 13:01:12 +0530

    existing class

--- src/main/java/com/example/Page3.java
+++ src/main/java/com/example/Page3.java
@@ -7,6 +7,7 @@ import org.testng.annotations.AfterMethod;
 public class Page3 {
   @Test
   public void f() {
+	  System.out.println("P:rint my name ");
   }
   @BeforeMethod
   public void beforeMethod() {
